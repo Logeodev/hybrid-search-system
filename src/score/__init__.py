83a0d92e75f66fdecacc01f108ae2e2a244290a4
leo.logel <leo.logel@sdea.fr>
2026-02-19 13:58:28 +0100

    refactor: Remove optimize_fusion_weights from score module and reintroduce in search module

--- src/score/__init__.py
+++ src/score/__init__.py
@@ -1,5 +1,4 @@
 from .fusion import ScoreFusion
-from .optimize import optimize_fusion_weights
 from .performance import PerformanceMonitor
 
 __all__ = ["ScoreFusion", "optimize_fusion_weights", "PerformanceMonitor"]
--- src/search/__init__.py
+++ src/search/__init__.py
@@ -1,3 +1,5 @@
 from .hybrid_rag import HybridSearchSystem
+from .monitored_hybrid_rag import MonitoredHybridSearch
+from .optimize import optimize_fusion_weights
 
-__all__ = ["HybridSearchSystem"]
+__all__ = ["HybridSearchSystem", "MonitoredHybridSearch", "optimize_fusion_weights"]
